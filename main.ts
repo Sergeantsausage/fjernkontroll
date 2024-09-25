9fb053767a34f17633793cf157c0c552cfed825e
Sergeantsausage <innjak@randabergskolen.no>
2024-09-25 09:11:20 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(sender_id)
+})
+input.onButtonPressed(Button.B, function () {
+    sender_id += 1
+    if (sender_id2 > 11) {
+        sender_id2 = 6
+    }
+})
+let sender_id2 = 0
+let sender_id = 0
+radio.setGroup(42)
+sender_id = 11
+basic.forever(function () {
+    basic.showNumber(sender_id - 6)
+})
